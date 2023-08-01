57f809196cc907425c77e9d5d097c9527ea55300
Shehuadekunmi <sirkumyemo@gmail.com>
2023-08-01 21:38:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+$('.fa-bars').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.nav').toggleClass('nav-toggle')
+});
+
+$(window).on('load scroll', function(){
+    $('.fa-bars').removeClass('fa-times');
+    $('.nav').removeClassif()('nav-toggle')
+})
+if( $(window).scrollTop() > 10){
+    $('header').addClass('header-active');
+}else{
+    $('header').removeClass('header-active');
+}
+});
